552e16349c3f0be02fad5d52d2b615c25aacad7b
Kenneth Olwing <knth@cpan.org>
2017-01-26 20:43:20 +0100

    Move to 0.009

--- extras/Java/TAPGenerator/src/main/java/org/cpan/knth/TAPGenerator.java
+++ extras/Java/TAPGenerator/src/main/java/org/cpan/knth/TAPGenerator.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 /**
  * @author knth@cpan.org
- * @version 0.008
+ * @version 0.009
  * 
  *          <h1>Simple TAP generator</h1>
  * 
--- lib/App/TestOnTap.pm
+++ lib/App/TestOnTap.pm
@@ -5,7 +5,7 @@ use 5.010_001;
 use strict;
 use warnings;
 
-our $VERSION = '0.008';
+our $VERSION = '0.009';
 my $version = $VERSION;
 $VERSION = eval $VERSION;
 

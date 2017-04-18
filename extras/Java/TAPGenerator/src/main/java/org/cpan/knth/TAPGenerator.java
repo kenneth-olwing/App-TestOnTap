fb076e091e5bc60b17c2e618318b2f5c50a088ee
Kenneth Olwing <knth@cpan.org>
2017-04-18 21:25:20 +0200

    Release 0.031
    A recent Test::More fix changed a behavior (empty output line removed).
    
    Test and dependency updated.

--- extras/Java/TAPGenerator/src/main/java/org/cpan/knth/TAPGenerator.java
+++ extras/Java/TAPGenerator/src/main/java/org/cpan/knth/TAPGenerator.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 /**
  * @author knth@cpan.org
- * @version 0.030
+ * @version 0.031
  * 
  *          <h1>Simple TAP generator</h1>
  * 
--- lib/App/TestOnTap.pm
+++ lib/App/TestOnTap.pm
@@ -5,7 +5,7 @@ use 5.010_001;
 use strict;
 use warnings;
 
-our $VERSION = '0.030';
+our $VERSION = '0.031';
 my $version = $VERSION;
 $VERSION = eval $VERSION;
 
